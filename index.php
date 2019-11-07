955fe9d899aa6aa76543cad92ad9566363d25142
edilsonclaudinosilva <edilsonclaudinosilvaprojetos@gmail.com>
2019-11-07 13:14:15 -0300

    listagem e configuracoes

---
+++ index.php
@@ -0,0 +1,25 @@
+ 
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="vendor/css/bootstrap-reboot.min.css" />
+
+    <title>Agenda</title>
+  </head>
+  <body>
+    
+
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="vendor/js/bootstrap.min.js" ></script>
+    
+    <script src="vendor/js/bootstrap.min.js"></script>
+  </body>
+</html>
